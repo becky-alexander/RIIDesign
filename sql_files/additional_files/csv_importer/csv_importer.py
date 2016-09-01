2ab791c3e6afaab0017027550ae97a8719c3cec7
Ross Conklin <Geekavenger@gmail.com>
2016-09-01 12:19:55 -0500

    updated file location

--- sql_files/additional_files/csv_importer/csv_importer.py
+++ sql_files/additional_files/csv_importer/csv_importer.py
@@ -6,8 +6,8 @@ conn = psycopg2.connect(conn_string)
 cursor = conn.cursor()
 
 
-with open('city_metrics_demographicdata_county.csv', 'rb') as f:
+with open('city_metrics_water_input.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         print row
-COPY city_metrics_demographicdata_county.csv 'M:\10Proj\100169\500 Technical\503 Calculations\sql files\additional_files\csv_importer\city_metrics_demographicdata_county.csv' DELIMITER ',' CSV HEADER;
+COPY city_metrics_water_input.csv '\sql_files\additional_files\csv_importer\city_metrics_water_input.csv' DELIMITER ',' CSV HEADER;
