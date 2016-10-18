1bf60ccbebc62340c01277a2c1b088fe795c0b24
Ross Conklin <Geekavenger@gmail.com>
2016-10-18 14:24:27 -0500

    Updating User model import for new version of Django

--- filebrowser/decorators.py
+++ filebrowser/decorators.py
@@ -2,7 +2,7 @@
 
 # django imports
 from django.shortcuts import get_object_or_404
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.conf import settings
 
 
