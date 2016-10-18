1f4c567ad261bc52cef7567ee0b222445271638e
Ross Conklin <Geekavenger@gmail.com>
2016-10-18 11:08:06 -0500

    Indent error??

--- city_metrics/models.py
+++ city_metrics/models.py
@@ -15,7 +15,7 @@ CITY_TYPES = (
 class City(models.Model):
 	id = models.IntegerField(primary_key=True)
 	city = models.CharField(max_length=100)
-	county_id = models.ForeignKey(county, db_column='id')
+	county_id = models.ForeignKey(County, db_column='id')
 	state = models.CharField(max_length=100)
 	type = models.CharField(max_length=100, choices=CITY_TYPES)
 	sort_order = models.IntegerField()
@@ -274,11 +274,12 @@ class CityMetricsCounty(models.Model):
     id = models.IntegerField(primary_key=True)
     county = models.CharField(max_length=50, blank=True, help_text='Ex. Hennepin')
     class Meta:
-		verbose_name = 'county'
-		verbose_name_plural = 'counties'
         db_table = u'city_metrics_county'
+		verbose_name = 'County'
+		verbose_name_plural = 'Counties'
+
 	def __unicode__(self):
-		return " %s" % (self.county)
+	return " %s" % (self.county)
 
 class CityMetricsElectricInput(models.Model):
 #    id = models.IntegerField(primary_key=True)
