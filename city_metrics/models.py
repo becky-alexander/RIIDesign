1fcdfa66c401c44ea3ec9e41fee1002746bb254b
Ross Conklin <Geekavenger@gmail.com>
2016-10-18 11:37:17 -0500

    Def County won't work

--- city_metrics/models.py
+++ city_metrics/models.py
@@ -276,7 +276,7 @@ class CityMetricsCounty(models.Model):
     class Meta:
         db_table = u'city_metrics_county'
 
-	def __unicode__(self):
+    def __unicode__(self):
 	return "%s" % (self.county)
 
 class CityMetricsElectricInput(models.Model):
