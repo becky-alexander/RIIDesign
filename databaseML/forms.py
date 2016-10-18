82711d960afdf62f0d1f794e1c138f50e1d1e134
Ross Conklin <Geekavenger@gmail.com>
2016-10-18 10:20:17 -0500

    Remove ID from Forms
    ID autoincrements and should not be entered by user so we removed it.

--- databaseML/forms.py
+++ databaseML/forms.py
@@ -12,12 +12,12 @@ from city_metrics.models import City, Energy, CityMetricsWaterInput, CityMetrics
 class CityForm(forms.ModelForm):
     class Meta:
         model = City
-        fields = ["id", "city", "county_id", "state", "type", "sort_order"]
+        fields = ["city", "county_id", "state", "type", "sort_order"]
 
 class EnergyForm(forms.ModelForm):
     class Meta:
         model = Energy
-        fields = ["city", "id", "year", "electricity","natural_gas", "other"]
+        fields = ["city", "year", "electricity","natural_gas", "other"]
 
 class CityMetricsWaterInputForm(forms.ModelForm):
     class Meta:
@@ -27,18 +27,18 @@ class CityMetricsWaterInputForm(forms.ModelForm):
 class CityMetricsEnergyUtilitiesForm(forms.ModelForm):
     class Meta:
         model = CityMetricsEnergyUtilities
-        fields = ["id", "name"]
+        fields = ["name"]
 
 class CityMetricsCountyForm(forms.ModelForm):
     class Meta:
         model = CityMetricsCounty
-        fields = ["id", "county"]
+        fields = ["county"]
 
 
 class CityMetricsElectricInputForm(forms.ModelForm):
     class Meta:
         model = CityMetricsElectricInput
-        fields = ["id", "city_id", "year", "utility_id1", "residential_electricity",
+        fields = ["city_id", "year", "utility_id1", "residential_electricity",
         "com_and_ind_electricity", "residential_wind_electricity", "com_and_ind_electricity", "utility_id2",
         "residential_electricity2", "com_and_ind_electricity2", "residential_wind_electricity2", "com_and_ind_wind_electricity2",
         "utility_id3", "residential_electricity3",
@@ -47,11 +47,11 @@ class CityMetricsElectricInputForm(forms.ModelForm):
 class CityMetricsNatGasInputForm(forms.ModelForm):
     class Meta:
         model = CityMetricsNatGasInput
-        fields = ["id", "city_id", "year", "utility_ida", "residential_nat_gasa", "com_and_ind_nat_gasa",
+        fields = ["city_id", "year", "utility_ida", "residential_nat_gasa", "com_and_ind_nat_gasa",
         "utility_idb", "residential_nat_gasb", "com_and_ind_nat_gasb", "utility_idc", "residential_nat_gasc",
         "com_and_ind_nat_gasc"]
 
 class CityMetricsEnergyUtilitiesEmissionFactorsForm(forms.ModelForm):
     class Meta:
         model = CityMetricsEnergyUtilitiesEmissionFactors
-        fields = ["id", "year", "utility", "type", "co2_ef", "n2o_ef", "ch4_ef"]
+        fields = ["year", "utility", "type", "co2_ef", "n2o_ef", "ch4_ef"]
