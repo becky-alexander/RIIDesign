2fbbf5e28c51874fdba5228fc4d46c413ff3dd92
Mauricio Leon <leonx075@umn.edu>
2016-10-23 01:08:41 -0500

    g

--- sql_files/input_tables/energy/other/other_input.sql
+++ sql_files/input_tables/energy/other/other_input.sql
@@ -15,23 +15,23 @@ CREATE TABLE city_metrics_other_energy_input
   city_id INT REFERENCES city_metrics_city(id) NOT NULL,
   year VARCHAR(4) NOT NULL,
 
-  other_fuel_type1 INT REFERENCES city_metrics_energy_utilities(id) NOT NULL,
+  other_fuel_type1 INT REFERENCES city_metrics_other_energy_type(id) NOT NULL,
   other_fuel_amount1_residential NUMERIC (16,4) NOT NULL,
   other_fuel_amount1_com_and_ind NUMERIC (16,4) NOT NULL,
 
-  other_fuel_type2 INT REFERENCES city_metrics_energy_utilities(id) NOT NULL,
+  other_fuel_type2 INT REFERENCES city_metrics_other_energy_type(id) NOT NULL,
   other_fuel_amount2_residential NUMERIC (16,4) NOT NULL,
   other_fuel_amount2_com_and_ind NUMERIC (16,4) NOT NULL,
 
-  other_fuel_type3 INT REFERENCES city_metrics_energy_utilities(id) NOT NULL,
+  other_fuel_type3 INT REFERENCES city_metrics_other_energy_type(id) NOT NULL,
   other_fuel_amount3_residential NUMERIC (16,4) NOT NULL,
   other_fuel_amount3_com_and_ind NUMERIC (16,4) NOT NULL,
 
-  other_fuel_type4 INT REFERENCES city_metrics_energy_utilities(id) NOT NULL,
+  other_fuel_type4 INT REFERENCES city_metrics_other_energy_type(id) NOT NULL,
   other_fuel_amount4_residential NUMERIC (16,4) NOT NULL,
   other_fuel_amount4_com_and_ind NUMERIC (16,4) NOT NULL,
 
-  other_fuel_type5 INT REFERENCES city_metrics_energy_utilities(id) NOT NULL,
+  other_fuel_type5 INT REFERENCES city_metrics_other_energy_type(id) NOT NULL,
   other_fuel_amount5_residential NUMERIC (16,4) NOT NULL,
   other_fuel_amount5_com_and_ind NUMERIC (16,4) NOT NULL
 
