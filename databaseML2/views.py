df3367a2d1df6dbf481d558780282c76e3840192
Mauricio Leon <leonx075@umn.edu>
2016-11-02 11:53:14 -0500

    Update views.py

--- databaseML2/views.py
+++ databaseML2/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
 from django.views import generic
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from city_metrics.models import *
