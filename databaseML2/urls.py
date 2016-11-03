2e768ef496db7bec2e0ec8eb1eeb2fa0d2536e99
Mauricio Leon <leonx075@umn.edu>
2016-11-02 22:01:18 -0500

    Update urls.py

--- databaseML2/urls.py
+++ databaseML2/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     #/databaseML2/waterinput/add/
-    url(r'waterinput/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    url(r'waterinput/add/$', views.WaterInputCreate.as_view(), name='water_input-add'),
     #/databaseML2/waterinput/2/
     url(r'waterinput/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
     #/databaseML2/waterinput/delete/
