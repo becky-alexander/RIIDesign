914d878e1645df44800d013924d9fd1c1953dd0c
Mauricio Leon <leonx075@umn.edu>
2016-11-02 19:36:15 -0500

    Update urls.py

--- databaseML2/urls.py
+++ databaseML2/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "databaseML2"
 
-urlpatterns = patterns['',
+urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     #/databaseML2/waterinput/add/
