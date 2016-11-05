2dc280993b7f0fa7043c72e2b07175d607ad1410
Mauricio Leon <leonx075@umn.edu>
2016-11-05 12:34:01 -0500

    all

--- databaseML2/urls.py
+++ databaseML2/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='nat_gas-index'),
     url(r'^nat_gasinput/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='nat_gas_input-detail'),
     #/databaseML2/nat_gasinput/add/
-    url(r'nat_gasinput/add/$', views.Nat_gasInputCreate.as_view(), name='nat_gas_input-add'),
+    url(r'nat_gasinput/add/$', views.NatGasInputCreate.as_view(), name='nat_gas_input-add'),
     #/databaseML2/nat_gasinput/update/2/
     url(r'nat_gasinput/update/(?P<pk>[0-9]+)/$', views.nat_gasInputUpdate.as_view(), name='nat_gas_input-update'),
     #/databaseML2/nat_gasinput/delete/
