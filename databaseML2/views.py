79830e6b8d0386cfead57ff8875a62a847e966e1
Mauricio Leon <leonx075@umn.edu>
2016-11-07 08:46:25 -0600

    all

--- databaseML2/views.py
+++ databaseML2/views.py
@@ -3,9 +3,10 @@ from django.views import generic
 from django.http import HttpResponse
 from django.views.generic import TemplateView,ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy, reverse
 from city_metrics.models import *
 from django.shortcuts import render
+
 
 
 def database2(request):
