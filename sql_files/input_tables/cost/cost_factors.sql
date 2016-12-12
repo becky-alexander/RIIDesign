f772287bf774e8ae313c0296ea36992924510273
Mauricio Leon <leonx075@umn.edu>
2016-12-12 11:05:24 -0600

    update_on_models

---
+++ sql_files/input_tables/cost/cost_factors.sql
@@ -0,0 +1,43 @@
+CREATE TABLE city_metrics_cost_factors
+
+(
+id SERIAL PRIMARY KEY,
+year VARCHAR(4),
+
+/*Electricity*/
+residential_electricity NUMERIC (16,4),
+commerical_electricity NUMERIC (16,4),
+industrial_electricity NUMERIC (16,4),
+
+
+/*Natural Gas*/
+residential_nat_gas NUMERIC (16,4),
+commerical_nat_gas NUMERIC (16,4),
+industrial_nat_gas NUMERIC (16,4),
+
+/*VMT*/
+travel_total_fuel_cost
+
+
+/*Solid Waste Management*/
+solid_waste_processed NUMERIC (16,4),
+solid_waste_landfilled NUMERIC (16,4),
+solid_waste_recycled NUMERIC (16,4),
+
+/*Potable Water Production and Distribution*/
+electricity_consumer_per_gallon_water NUMERIC (16,4),
+cost_per_gallon_water NUMERIC (16,4),
+
+/*Waste Water Treatment*/
+wastewater_electricity NUMERIC (16,4),
+wastewater_natural_gas NUMERIC (16,4),
+wastewater_fuel_oil NUMERIC (16,4),
+
+coal NUMERIC (16,4),
+
+
+
+
+/**/
+
+COPY city_metrics_cost_factor_electricity FROM 'C:/csv_files/city_metrics_cost_factor_electricity.csv' DELIMITER ',' CSV HEADER;
