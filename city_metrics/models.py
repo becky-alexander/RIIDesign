c595820b2519ddce588da1d45b308ca54ed9992c
Mauricio Leon <leonx075@umn.edu>
2016-12-12 11:14:40 -0600

    Update models.py

--- city_metrics/models.py
+++ city_metrics/models.py
@@ -23,7 +23,7 @@ class CityMetricsCounty(models.Model):
 class City(models.Model):
 	id = models.IntegerField(primary_key=True)
 	city = models.CharField(max_length=100)
-	county_id = models.ForeignKey(CityMetricsCounty, db_column='id', verbose_name='County')
+	county_id = models.ForeignKey(CityMetricsCounty, db_column='county_id', verbose_name='County')
 	state = models.CharField(max_length=100)
 	type = models.CharField(max_length=100, choices=CITY_TYPES)
 	sort_order = models.IntegerField()
