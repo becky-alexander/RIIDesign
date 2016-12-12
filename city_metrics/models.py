fdccb448476c5307cb0184d856c896342782956c
Mauricio Leon <leonx075@umn.edu>
2016-12-11 20:09:50 -0600

    Update models.py

--- city_metrics/models.py
+++ city_metrics/models.py
@@ -174,10 +174,15 @@ class CityMetricsWaterInput(models.Model):
     def __unicode__(self):
 	return " %s -- %s" % (self.city_id, self.year)
 
+UTILITY_TYPES = (
+	('Electric', 'Electric'),
+	('Natural Gas', 'Natural Gas'),
+)
+
 class CityMetricsEnergyUtilities(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, verbose_name='Utility Name', unique=True)
-    type = models.CharField(max_length=50, verbose_name='Type')
+    type = models.CharField(max_length=50, verbose_name='Type', choices='UTILITY_TYPES')
     class Meta:
         db_table = u'city_metrics_energy_utilities'
 	verbose_name = 'Energy Utility'
