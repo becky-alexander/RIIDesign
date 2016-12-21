f7d09134600424bfc21251e0757f89a2f885d34b
Mauricio Leon <leonx075@umn.edu>
2016-12-21 10:10:12 -0600

    Create intermediary_cost_water.sql

---
+++ sql_files/intermediary_views/cost/intermediary_cost_water.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW intermediary_cost_water AS
+SELECT(
+city_metrics_cost_factors.electricity_consumed_kwh_per_gallon_water,
+city_metrics_cost_factors.cost_per_gallon_water,
+
+city_metrics_water.total_res_water,
+city_metrics_water.total_com_ind_water,
+
+(city_metrics_water.total_res_water * city_metrics_cost_factors.electricity_consumed_kwh_per_gallon_water),
+(city_metrics_water.total_res_water * city_metrics_cost_factors.cost_per_gallon_water),
+
+city_metrics_water.total_com_ind_water,
+
+)
+FROM city_metrics_cost_factors, city_metrics_water
+WHERE city_metrics_cost_factors.year = city_metrics_water.year
