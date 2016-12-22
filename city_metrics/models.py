53810821d3bee6efeca1fdfab06bc9772292a7ae
Mauricio Leon <leonx075@umn.edu>
2016-12-21 20:32:10 -0600

    Update models.py

--- city_metrics/models.py
+++ city_metrics/models.py
@@ -201,13 +201,13 @@ class CityMetricsElectricInput(models.Model):
     residential_wind_electricity = models.DecimalField(max_digits=16, blank=True, default=0.00, decimal_places=4, help_text = "kWh", verbose_name='Residential Electricity (wind generated)')
     com_and_ind_wind_electricity = models.DecimalField(max_digits=16, blank=True, default=0.00, decimal_places=4, help_text = "kWh", verbose_name='Commericial & Industrial Electricity (wind generated)')
     # ELECTRICITY UTILITY 2
-    utility_id2 = models.ForeignKey(CityMetricsEnergyUtilities, related_name="Utility II", default='NONE', verbose_name='Utility #2', db_column='utility_id2')
+    utility_id2 = models.ForeignKey(CityMetricsEnergyUtilities, null=True, blank=True, related_name="Utility II", default='NONE', verbose_name='Utility #2', db_column='utility_id2')
     residential_electricity2 = models.DecimalField(max_digits=16, decimal_places=4, blank=True, default=0.00, help_text = "kWh", verbose_name='Total Residential Electricity')
     com_and_ind_electricity2 = models.DecimalField(max_digits=16, decimal_places=4, blank=True, default=0.00, help_text = "kWh", verbose_name='Total Commericial & Industrial Electricity')
     residential_wind_electricity2 = models.DecimalField(blank=True, default=0.00, max_digits=16, decimal_places=4, help_text = "kWh", verbose_name='Residential Electricity (wind generated)')
     com_and_ind_wind_electricity2 = models.DecimalField(blank=True, default=0.00, max_digits=16, decimal_places=4, help_text = "kWh", verbose_name='Commericial & Industrial Electricity (wind generated)')
     # ELECTRICITY UTILITY 3
-    utility_id3 = models.ForeignKey(CityMetricsEnergyUtilities, related_name="Utility III", default='NONE', verbose_name='Utility #3', db_column='utility_id3')
+    utility_id3 = models.ForeignKey(CityMetricsEnergyUtilities, null=True, blank=True, related_name="Utility III", default='NONE', verbose_name='Utility #3', db_column='utility_id3')
     residential_electricity3 = models.DecimalField(blank=True, max_digits=16, decimal_places=4, default=0.00, help_text = "kWh", verbose_name='Total Residential Electricity')
     com_and_ind_electricity3 = models.DecimalField(blank=True, max_digits=16, decimal_places=4, default=0.00, help_text = "kWh", verbose_name='Total Commericial & Industrial Electricity')
     residential_wind_electricity3 = models.DecimalField(blank=True, max_digits=16, decimal_places=4, default=0.00, help_text = "kWh", verbose_name='Residential Electricity (wind generated)')
