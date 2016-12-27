947263cdac468529ea384b6b1ecd30f07522b35a
Mauricio Leon <leonx075@umn.edu>
2016-12-26 23:46:15 -0600

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -198,7 +198,7 @@ SUIT_CONFIG = {
 	'MENU': (
 		{'app': 'auth'},
 		{'app': 'city_metrics', 'label': 'City Metrics', 'icon': 'icon-signal'},
-		{'app': 'city_metrics', 'label': 'Metrics', 'icon': 'icon-signal', 'models': ('CityMetricsWaterInput')},
+		{'app': 'city_metrics', 'label': 'Metrics', 'icon': 'icon-signal', 'models': ('CityMetricsWaterInput', 'CityMetricsElectricInput',)},
 #		{'app': 'site_content', 'label': 'Site Content', 'icon': 'icon-file'},
 #		{'label': 'Input Forms', 'icon': 'icon-signal', 'url': '/database/'},
 #		{'label': 'Site Media', 'icon': 'icon-folder-open', 'url': '/admin/filebrowser/browse/'},
